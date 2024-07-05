d289c47cedb795484b59d3d19c3fc5a3102d92bc
adityaoncloud <adityaoncloud25@gmail.com>
2024-07-05 20:01:46 +0530

    added cards ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,121 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from './components/ui/card'
 import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='grid grid-cols-5 gap-4'>
+        <div>
+        <Card>
+          <CardHeader>
+          <CardTitle>Title Here</CardTitle>
+          <CardDescription>Description Here</CardDescription>
+          </CardHeader>
+          <CardContent>
+            Status: Verified
+            <Button>Verify</Button>
+          </CardContent>
+          <CardFooter>
+            <Button>Revoke Access</Button>
+          </CardFooter>
+        </Card>
+        </div>
+        <div>
+        <Card>
+          <CardHeader>
+          <CardTitle>Title Here</CardTitle>
+          <CardDescription>Description Here</CardDescription>
+          </CardHeader>
+          <CardContent>
+            Status: Verified
+            <Button>Verify</Button>
+          </CardContent>
+          <CardFooter>
+            <Button>Revoke Access</Button>
+          </CardFooter>
+        </Card>
+        </div>
+        <div>
+        <Card>
+          <CardHeader>
+          <CardTitle>Title Here</CardTitle>
+          <CardDescription>Description Here</CardDescription>
+          </CardHeader>
+          <CardContent>
+            Status: Verified
+            <Button>Verify</Button>
+          </CardContent>
+          <CardFooter>
+            <Button>Revoke Access</Button>
+          </CardFooter>
+        </Card>
+        </div>
+        <div>
+        <Card>
+          <CardHeader>
+          <CardTitle>Title Here</CardTitle>
+          <CardDescription>Description Here</CardDescription>
+          </CardHeader>
+          <CardContent>
+            Status: Verified
+            <Button>Verify</Button>
+          </CardContent>
+          <CardFooter>
+            <Button>Revoke Access</Button>
+          </CardFooter>
+        </Card>
+        </div>
+        <div>
+        <Card>
+          <CardHeader>
+          <CardTitle>Title Here</CardTitle>
+          <CardDescription>Description Here</CardDescription>
+          </CardHeader>
+          <CardContent>
+            Status: Verified
+            <Button>Verify</Button>
+          </CardContent>
+          <CardFooter>
+            <Button>Revoke Access</Button>
+          </CardFooter>
+        </Card>
+        </div>
+        <div>
+        <Card>
+          <CardHeader>
+          <CardTitle>Title Here</CardTitle>
+          <CardDescription>Description Here</CardDescription>
+          </CardHeader>
+          <CardContent>
+            Status: Verified
+            <Button>Verify</Button>
+          </CardContent>
+          <CardFooter>
+            <Button>Revoke Access</Button>
+          </CardFooter>
+        </Card>
+        </div>
+        <div>
+        <Card>
+          <CardHeader>
+          <CardTitle>Title Here</CardTitle>
+          <CardDescription>Description Here</CardDescription>
+          </CardHeader>
+          <CardContent>
+            Status: Verified
+            <Button>Verify</Button>
+          </CardContent>
+          <CardFooter>
+            <Button>Revoke Access</Button>
+          </CardFooter>
+        </Card>
+        </div>
+        
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
