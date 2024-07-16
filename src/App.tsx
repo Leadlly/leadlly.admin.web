4cc419faa5f051c232786d0ed7c1d9394413bc29
ShivaniYadav07 <sagaryadavarmy813@gmail.com>
2024-07-16 22:14:56 +0530

    initialise components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from './components/ui/card'
+// import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from './components/ui/card'
 import './App.css'
-import { Button } from './components/ui/button'
+// import { Button } from './components/ui/button'
+import SignUp from './components/Auth/SignUp'
 
 function App() {
  
@@ -8,111 +9,7 @@ function App() {
   return (
     <>
       <div className='grid grid-cols-5 gap-4'>
-        <div>
-        <Card>
-          <CardHeader>
-          <CardTitle>Title Here</CardTitle>
-          <CardDescription>Description Here</CardDescription>
-          </CardHeader>
-          <CardContent>
-            Status: Verified
-            <Button>Verify</Button>
-          </CardContent>
-          <CardFooter>
-            <Button>Revoke Access</Button>
-          </CardFooter>
-        </Card>
-        </div>
-        <div>
-        <Card>
-          <CardHeader>
-          <CardTitle>Title Here</CardTitle>
-          <CardDescription>Description Here</CardDescription>
-          </CardHeader>
-          <CardContent>
-            Status: Verified
-            <Button>Verify</Button>
-          </CardContent>
-          <CardFooter>
-            <Button>Revoke Access</Button>
-          </CardFooter>
-        </Card>
-        </div>
-        <div>
-        <Card>
-          <CardHeader>
-          <CardTitle>Title Here</CardTitle>
-          <CardDescription>Description Here</CardDescription>
-          </CardHeader>
-          <CardContent>
-            Status: Verified
-            <Button>Verify</Button>
-          </CardContent>
-          <CardFooter>
-            <Button>Revoke Access</Button>
-          </CardFooter>
-        </Card>
-        </div>
-        <div>
-        <Card>
-          <CardHeader>
-          <CardTitle>Title Here</CardTitle>
-          <CardDescription>Description Here</CardDescription>
-          </CardHeader>
-          <CardContent>
-            Status: Verified
-            <Button>Verify</Button>
-          </CardContent>
-          <CardFooter>
-            <Button>Revoke Access</Button>
-          </CardFooter>
-        </Card>
-        </div>
-        <div>
-        <Card>
-          <CardHeader>
-          <CardTitle>Title Here</CardTitle>
-          <CardDescription>Description Here</CardDescription>
-          </CardHeader>
-          <CardContent>
-            Status: Verified
-            <Button>Verify</Button>
-          </CardContent>
-          <CardFooter>
-            <Button>Revoke Access</Button>
-          </CardFooter>
-        </Card>
-        </div>
-        <div>
-        <Card>
-          <CardHeader>
-          <CardTitle>Title Here</CardTitle>
-          <CardDescription>Description Here</CardDescription>
-          </CardHeader>
-          <CardContent>
-            Status: Verified
-            <Button>Verify</Button>
-          </CardContent>
-          <CardFooter>
-            <Button>Revoke Access</Button>
-          </CardFooter>
-        </Card>
-        </div>
-        <div>
-        <Card>
-          <CardHeader>
-          <CardTitle>Title Here</CardTitle>
-          <CardDescription>Description Here</CardDescription>
-          </CardHeader>
-          <CardContent>
-            Status: Verified
-            <Button>Verify</Button>
-          </CardContent>
-          <CardFooter>
-            <Button>Revoke Access</Button>
-          </CardFooter>
-        </Card>
-        </div>
+       <SignUp/>
         
       </div>
       
